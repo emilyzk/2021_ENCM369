8b88a0cb35f303e770d0f4c051a6fee895ccf0ad
Emily <emilyzoeknight@gmail.com>
2021-02-25 13:31:02 -0700

    Revert "PIC Activity 4 Code Files"
    This reverts commit 9235a85f60225ebd99587b3ee45b965664a3f438.

--- user_app.p1.d
+++
@@ -1,7 +0,0 @@
-build/default/production/_ext/1942967838/user_app.p1:  \
-../../Git/2021_ENCM369/user_app.c  \
-../../Git/2021_ENCM369/configuration.h  \
-../../Git/2021_ENCM369/typedefs.h  \
-../../Git/2021_ENCM369/main.h  \
-../../Git/2021_ENCM369/encm369_pic18.h  \
-../../Git/2021_ENCM369/user_app.h 
